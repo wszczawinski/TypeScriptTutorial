88af915a548609c7176095f5eeb82d4d814bd7b2
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-06-30 21:36:59 +0200

    feat(modules)

--- public/app.js
+++ public/app.js
@@ -1,42 +1,28 @@
-"use strict";
-var Invoice = /** @class */ (function () {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes " + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice('Bob', 'Glasses', 50);
-var invTwo = new Invoice('John', 'Glasses', 30);
-var invoices = [];
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('Bob', 'Glasses', 50);
+const invTwo = new Invoice('John', 'Glasses', 30);
+let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invoices.forEach(function (inv) {
+invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
 });
-var anchor = document.querySelector('a');
+const anchor = document.querySelector('a');
 //if developer is sure that there is some anchor tag in html file
 //can add '!' which will stap tsc from showing an error
 // if (anchor) {
 //     console.log(anchor);
 // }
 console.log(anchor.href);
-var form1 = document.querySelector('form');
-var form2 = document.querySelector('.new-item-form');
+const form1 = document.querySelector('form');
+const form2 = document.querySelector('.new-item-form');
 // console.log(form1.children);
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form2.addEventListener('submit', function (e) {
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form2.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,4 @@
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number
-    ) {}
-
-    format() {
-        return `${this.client} owes ${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('Bob', 'Glasses', 50);
 const invTwo = new Invoice('John', 'Glasses', 30);
